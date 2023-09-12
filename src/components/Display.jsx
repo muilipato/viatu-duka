dded26497dafa43997a210e5ae009104b7806b30
Patrick <muilipato@gmail.com>
2023-09-12 21:43:14 +0300

    added shoe items

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -11,37 +11,52 @@ export default function Display(){
         const fakeData = [
             {
                 name: "shoe 1",
-                brand: "brand 1",
+                brand: "Nike",
                 price: 6000,
-                imageUrl: ''
+                imageUrl:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+
 
             },
             {
                 name:"shoe 2",
-                brand: "shoe 3",
+                brand: "Adidas",
                 price: 6000,
-                imageurl:''
+                imageUrl:'https://images.unsplash.com/photo-1508609349937-5ec4ae374ebf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1659&q=80'
 
             },
             {
-                name:"shoe 2",
-                brand: "shoe 3",
+                name:"shoe 3",
+                brand: "Nike",
                 price: 6000,
-                imageurl:''
+                imageUrl:'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
 
             },
             {
-                name:"shoe 2",
-                brand: "shoe 3",
+                name:"shoe 4",
+                brand: "Nike",
                 price: 6000,
-                imageurl:''
+                imageUrl:'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
 
             },
             {
-                name:"shoe 2",
-                brand: "shoe 3",
+                name:"shoe 5",
+                brand: "Nike",
                 price: 6000,
-                imageurl:''
+                imageUrl:'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
+
+            },
+            {
+                name:"shoe 6",
+                brand: "Nike",
+                price: 6000,
+                imageUrl:'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
+
+            },
+            {
+                name:"shoe 7",
+                brand: "Nike",
+                price: 6000,
+                imageUrl:'https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80'
 
             }
         ];
@@ -58,8 +73,8 @@ export default function Display(){
             <div className="shoes-container">
                 <div className="items-container">
                 {shoeData.map((shoe, index)=>
-             <ShoeItem key={index} shoeData={shoe}/>
-             )}
+                 <ShoeItem key={index} shoeData={shoe}/>
+                 )}
 
                 </div>
 
